37bc4a90c2e520af4adf4e38d87600a19de7e775
ronozoro <m.dev.odoo@gmail.com>
2021-06-25 20:50:48 +0200

    add required js files and build api connection to get data from kaggle

--- src/main/java/com/wuzzuf/controller/LandingAnalyzer.java
+++ src/main/java/com/wuzzuf/controller/LandingAnalyzer.java
@@ -1,5 +1,13 @@
 package com.wuzzuf.controller;
 
-public class LandingAnalyzer {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class LandingAnalyzer {
+    @GetMapping("/")
+    public String homepage(Model model) {
+        return "analyzer_home";
+    }
 }
--- src/main/java/com/wuzzuf/service/LoadDataset.java
+++ src/main/java/com/wuzzuf/service/LoadDataset.java
@@ -1,4 +1,56 @@
 package com.wuzzuf.service;
 
+import org.apache.tomcat.util.codec.binary.Base64;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import com.wuzzuf.model.WuzzufJobs;
+import static com.wuzzuf.service.Annotation.*;
+
 public class LoadDataset {
+
+    public void get_dataset_list(String dataset_name) {
+        LoadDataset ds = new LoadDataset();
+        ds.download_csv(dataset_name);
+    }
+
+    public void download_csv(String dataset_name) {
+        String dataset_path = KAGGLE_API_BASE_URL;
+        try {
+
+            URL direct = new URL(dataset_path);
+            HttpURLConnection urlConnection = null;
+            urlConnection = (HttpURLConnection) direct.openConnection();
+            urlConnection.setRequestMethod("GET");
+            String auth = defaultKName + ":" + defaultKPass;
+            byte[] encodeAuth = Base64.encodeBase64(
+                    auth.getBytes(StandardCharsets.ISO_8859_1)
+            );
+            String authHeaderValue = "Basic " + new String(encodeAuth);
+            urlConnection.setRequestProperty("User-Agent", "Mozilla/5.0");
+            urlConnection.setRequestProperty("Authorization", authHeaderValue);
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(urlConnection.getInputStream()));
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine + "\n");
+            }
+            in.close();
+            BufferedReader bufReader = new BufferedReader(new StringReader(response.toString()));
+            String line = null;
+            FileWriter file = new FileWriter(KAGGLE_Path_DOWNLOAD +dataset_name+ ".csv");
+            PrintWriter write = new PrintWriter(file);
+            while ((line = bufReader.readLine()) != null) {
+                write.println(line);
+            }
+            write.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
